a88f11899aefef27bd5c88813189df9e02eec6b8
rodriggj <gabriel.rodriguez.ctr@gmail.com>
2022-04-02 13:32:14 -0400

    commit from plane

---
+++ homework/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button id="schedule-editor">Launch Schedule Editor</button>  
+      </header>
+    </div>
+  );
+}
+
+export default App;
