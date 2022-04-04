604e69295078a85fff452c3d702ea7d28d30d7a9
rodriggj <gabriel.rodriguez.ctr@gmail.com>
2022-04-04 08:50:12 -0600

    adding config.js file

---
+++ config.js
@@ -0,0 +1,8 @@
+// Rename this file to config.js and then add your client secret
+const config = {};
+
+config.nylasClientSecret = '3xu91un2h1845zq4eyqkhtjki';
+config.host = '127.0.0.1';
+config.port = 3000;
+
+module.exports = config;
